d638aa077f3444027a29ecba44769cfaead2071e
Abhishek kumar <abhishekkumargond592@gmail.com>
2025-04-25 02:06:51 +0530

    YogaStream

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,6 +29,7 @@ import ResetPassword from "./components/auth/ResetPassword";
 import QuoteMarquee from "./components/quoteTicker/QuoteTicker";
 import Review from "./components/reviews/Review";
 import Task from "./components/task/Task";
+import YogaStream from "./components/yogastreaming/YogaStream";
 
 // import YogaStream from "./components/yogastreaming/YogaStream";
 
